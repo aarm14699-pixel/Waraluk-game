ee09bb1c7261a64d5b3c4d2c6e775f55e4d5b7c3
aarm14699-pixel <aarm14699@gmail.com>
2026-09-23 12:34:10 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,1 @@
+export default {content:['./index.html','./src/**/*.{js,jsx}'],theme:{extend:{colors:{navy:'#101f30',cream:'#fff0d5',amber:'#ffc977'},fontFamily:{rounded:['Nunito','Noto Sans Thai','sans-serif']}}},plugins:[]};
